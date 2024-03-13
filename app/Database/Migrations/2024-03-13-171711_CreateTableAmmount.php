ca64b8036dc834bd1e5bc4bd83737f9492b55035
Kai Akkermann <probearbeit@kaapke.com>
2024-03-13 18:23:11 +0100

    Add Table Ammount

---
+++ app/Database/Migrations/2024-03-13-171711_CreateTableAmmount.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableAmmount extends Migration
+{
+    public function up()
+    {
+        $table = 'ammount';
+        $fields = [
+            'id' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto-increment'    => true,
+            ],
+            'user_id' => [
+                'type'              => 'INT',
+                'constraint'        => '11',
+                'unsigned'          => true,
+            ],
+            'ammount' => [
+                'type'              => 'FLOAT',
+                'constraint'        => '15',
+                'unsigned'          => true,
+                'default'           => 0,
+            ],
+            'zeit' => [
+                'type'              => 'DATETIME',
+                'null'              => true,
+            ],
+        ];
+
+        $this->forge->addField($fields);
+        $this->forge->addKey('id');
+        $this->forge->addForeignKey('user_id','users', 'id', 'CASCADE', 'CASCADE', 'my_user_ammount');
+        $this->forge->createTable($table);
+    }
+
+    public function down()
+    {
+        $table = 'ammount';
+        $this->forge->dropTable($table);
+    }
+}
